e5a8b6f17917e1ecb051cae6bf9cf576a5d201fc
Jerome Etienne <jerome.etienne@gmail.com>
2013-06-02 19:38:41 +0200

    New deploy

--- threex.transparency.js
+++ threex.transparency.js
@@ -8,23 +8,21 @@ THREEx.Transparency	= {};
 
 /**
  * init transparency of a object recursivly
- * @param  {THREE.Object3D} object the object3D to update
+ * @param  {THREE.Object3D[]} objects the object which are transparent
  */
-THREEx.Transparency.init	= function(object){
-	object.traverse(function(object){
-		var material	= object.material
-		if( !material )	return;
-		if( material.transparent !== true )	return;
-		material.depthWrite	= false
+THREEx.Transparency.init	= function(objects){
+	objects.forEach(function(object){
+		object.material.transparent	= true
+		object.material.depthWrite	= false
 	});	
 }
 
 /**
  * update the object for transparency rendering
- * @param  {THREE.Object3D} object the object3D to update
+ * @param  {THREE.Object3D[]} objects the objects which are transparent
  * @param  {THREE.Camera} camera the camera used for rendering
  */
-THREEx.Transparency.update	= function(object, camera){
+THREEx.Transparency.update	= function(objects, camera){
 	// update camera matrices
 	camera.updateMatrixWorld()
 	camera.matrixWorldInverse.getInverse( camera.matrixWorld )
@@ -32,20 +30,15 @@ THREEx.Transparency.update	= function(object, camera){
 	var screenMatrix= new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse)
 	var position	= new THREE.Vector3()
 	
-	// update the matrixWorld of the object and its children
-	object.updateMatrixWorld()
 	// traverse the object
-	object.traverse(function(childObject){
-		// ignore object which doesnt have material
-		if( childObject.material === undefined )		return
-		// ignore object which arent transparent
-		if( childObject.material.transparent === false )	return;
-		
+	objects.forEach(function(object){
+		// update the matrixWorld of the object and its children
+		object.updateMatrixWorld()
 		// compute its position in screen space 
-		position.getPositionFromMatrix( childObject.matrixWorld );
+		position.getPositionFromMatrix( object.matrixWorld );
 		position.applyProjection( screenMatrix );
 		// use the position.x as renderDepth
-		childObject.renderDepth	= position.z;
+		object.renderDepth	= position.z;
 	})
 }
 
